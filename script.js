7ef997f32c878880f47254b164de496e368368ea
Andrea Zapata <95andreazapata@gmail.com>
2023-01-31 20:14:58 -0500

    Completing displayWord function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const wordEl = document.getElementById('word');
-const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
-const popup = document.getElementById('popup-container');
-const notification = document.getElementById('notification-container');
-const finalMessage = document.getElementById('final-message');
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 
-const figureParts = document.querySelectorAll('.figure-part');
+const figureParts = document.querySelectorAll(".figure-part");
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -16,16 +16,27 @@ const wrongLetters = [];
 
 // Show the hidden word
 function displayWord() {
-    wordEl.innerHTML = `
+  wordEl.innerHTML = `
     ${selectedWord
-        .split('')
-        .map(letter => `
+      .split("")
+      .map(
+        (letter) => `
         <span class="letter">
-        ${correctLetters.includes(letter) ? letter : ''}
+        ${correctLetters.includes(letter) ? letter : ""}
         </span>
-        `).join('')
-    }
+        `
+      )
+      .join("")}
     `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+    if (innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won! :)';
+        popup.style.display = 'flex';
+    }
+
+    console.log(wordEl.innerText, innerWord);
 }
 
 displayWord();
